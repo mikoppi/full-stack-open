95f8f914b26f7500361d150ebce8c4837f3f234f
Miko Peltokorpi <miko.peltokorpi@gmail.com>
2022-08-24 16:16:06 +0300

    Finish unicafe submission

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -1,25 +1,111 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+);
+
+const StatisticsLine = ({ text, value }) => (
+  <div>
+    <p>
+      {text} {value}
+    </p>
+  </div>
+);
+
+const Statistics = ({ good, neutral, bad }) => {
+  if (good === 0 && neutral === 0 && bad === 0) {
+    return (
+      <div>
+        <h1>statistics</h1>
+        <p>No feedback given</p>
+      </div>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <table>
+        <tbody>
+          <tr>
+            <td>
+              <h1>statistics</h1>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <StatisticsLine text="good" value={good} />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <StatisticsLine text="neutral" value={neutral} />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <StatisticsLine text="bad" value={bad} />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <StatisticsLine text="all" value={good + neutral + bad} />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <StatisticsLine
+                text="average"
+                value={(good + bad * -1) / (good + neutral + bad)}
+              />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <StatisticsLine
+                text="positive"
+                value={(good / (good + neutral + bad)) * 100 + " %"}
+              />
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
-}
+};
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  return (
+    <div>
+      <table>
+        <tbody>
+          <tr>
+            <td>
+              <h1>give feedback</h1>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <Button handleClick={() => setGood(good + 1)} text="good" />
+              <Button
+                handleClick={() => setNeutral(neutral + 1)}
+                text="neutral"
+              />
+              <Button handleClick={() => setBad(bad + 1)} text="bad" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <Statistics good={good} neutral={neutral} bad={bad} />
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 export default App;
