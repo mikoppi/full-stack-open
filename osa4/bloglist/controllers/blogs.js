0c5463d75a4b57c40b1962a687d9761951a6e977
Miko Peltokorpi <miko.peltokorpi@gmail.com>
2022-09-02 10:50:18 +0300

    Finish submissions 4.1.-4.2.

---
+++ osa4/bloglist/controllers/blogs.js
@@ -0,0 +1,28 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+
+//get all blogs
+blogsRouter.get('/', (request, response) => {
+    Blog
+      .find({})
+      .then(blogs => {
+        response.json(blogs)
+      })
+  })
+
+  
+//post one blog
+blogsRouter.post('/', (request, response) => {
+    const blog = new Blog(request.body)
+  
+    blog
+      .save()
+      .then(result => {
+        response.status(201).json(result)
+      })
+  })
+
+
+
+module.exports = blogsRouter
